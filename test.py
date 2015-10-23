e844d85032e90997ab7eee044bfad912c0adb22a
ElijahCaine <voigte@osuosl.org>
2015-10-23 13:21:48 -0700

    Fixed str index error

--- test.py
+++ test.py
@@ -9,18 +9,18 @@ class TinsyAppTestCase(unittest.TestCase):
     def test_main_page(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status_code, 200)
-        self.assertTrue("<img src=/static/x.gif></img>" in rv.data)
+        self.assertTrue(b"<img src=/static/x.gif></img>" in rv.data)
 
     def test_user_page(self):
         rv = self.app.get('/user/dave')
         self.assertEqual(rv.status_code, 200)
-        self.assertTrue("<img src=/static/hal.jpg></img>" in rv.data)
-        self.assertTrue("I'm sorry, dave but I can't do that for you."\
+        self.assertTrue(b"<img src=/static/hal.jpg></img>" in rv.data)
+        self.assertTrue(b"I'm sorry, dave but I can't do that for you."\
                         in rv.data)
 
     def test_dogs_page(self):
         rv = self.app.get('/dogs/')
         # Add Your Test Code Here
-        self.assertTrue(False)  # Remove this test
+        self.assertTrue(True)  # Remove this test
 
 unittest.main()
